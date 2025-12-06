1914ca262b515040e4b80c389bb9a81d69f4cf35
Enzo Samudio <enzosamudio@gmail.com>
2025-12-06 11:52:50 -0300

    correccion carga .env

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import fileParseRouter from './routes/file-parser.route';
 import { loadEnvFile } from 'node:process';
-loadEnvFile('.env');
 
+if (process.env.NODE_ENV !== 'production') {
+    loadEnvFile('.env'); 
+}
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '20mb' }));
