eb5e91d0d686c9a554210b425b9d09435282f16f
Enzo Samudio <enzosamudio@gmail.com>
2025-12-06 11:57:38 -0300

    eliminacion bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import fileParseRouter from './routes/file-parser.route';
 import { loadEnvFile } from 'node:process';
@@ -11,7 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '20mb' }));
 app.use(cors());
-app.use(bodyParser.json());
 app.use(fileParseRouter);
 
 app.listen(PORT, () => {
